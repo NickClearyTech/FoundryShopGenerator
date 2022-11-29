6f124c051e9a67b911e1c94d61ebb7ca894a106e
Nick Cleary <nicholascleary01@gmail.com>
2022-11-29 00:22:31 -0500

    making a commit

---
+++ run.sh
@@ -0,0 +1,3 @@
+#rm -r $HOME/Documents/FoundryTestData/Data/modules/shop-generator
+#cp -r shop-generator $HOME/Documents/FoundryTestData/Data/modules
+node $HOME/Documents/FoundryInstall/resources/app/main.js --dataPath=$HOME/Documents/FoundryTestData
