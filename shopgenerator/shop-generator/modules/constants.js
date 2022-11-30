76f9db9f330a48c5355017c1b0f768649c30946a
Nick Cleary <nicholascleary01@gmail.com>
2022-11-30 01:01:49 -0500

    Updates to potion shop setting

--- shopgenerator/shop-generator/modules/constants.js
+++ shopgenerator/shop-generator/modules/constants.js
@@ -4,9 +4,22 @@
 export default class Constants {
     static MODULE_ID = "shop-generator";
 
-    static valid_compendium_types = ["actor", "item", "card stack", "journal entry", "macro", "playlist", "rollable table", "scene", "adventure"];
+    static valid_compendium_types = [
+        "actor",
+        "item",
+        "card stack",
+        "journal entry",
+        "macro",
+        "playlist",
+        "rollable table",
+        "scene",
+        "adventure"
+    ];
 
     static settings = {
         potion_compendium: "PotionCompendium",
+        spell_compendium: "SpellCompendium",
+        magic_items_compendium: "MagicItemCompendium",
+        equipment_compendium: "EquipmentCompendium"
     }
 }
--- shopgenerator/shop-generator/modules/settings.js
+++ shopgenerator/shop-generator/modules/settings.js
@@ -12,14 +12,53 @@ export default class Settings {
         // A setting for the user to select which compendium to use for the potion shop generation.
         game.settings.register(Constants.MODULE_ID, Constants.settings.potion_compendium,
             {
-                name: `SHOP_GEN.settings.potion_compendium.name`,
+                name: "SHOP_GEN.settings.potion_compendium.name",
                 hint: "SHOP_GEN.settings.potion_compendium.hint",
                 scope: "client",
                 requiresReload: false,
                 type: String,
                 choices: getCompendiumsOfType("item"),
                 config: true,
-                default: true,
+                default: ""
+            }
+        );
+        // A setting for the user to select which compendium contains spells present in magic shops as scrolls/gems
+        game.settings.register(Constants.MODULE_ID, Constants.settings.spell_compendium,
+            {
+                name: "SHOP_GEN.settings.spell_compendium.name",
+                hint: "SHOP_GEN.settings.spell_compendium.hint",
+                scope: "client",
+                requiresReload: false,
+                type: String,
+                choices: getCompendiumsOfType("item"),
+                config: true,
+                default: ""
+            }
+        );
+        // A setting for the user to select which compendium contains magic items to be present in magic shops
+        game.settings.register(Constants.MODULE_ID, Constants.settings.magic_items_compendium,
+            {
+                name: "SHOP_GEN.settings.magic_item_compendium.name",
+                hint: "SHOP_GEN.settings.magic_item_compendium.hint",
+                scope: "client",
+                requiresReload: false,
+                type: String,
+                choices: getCompendiumsOfType("item"),
+                config: true,
+                default: ""
+            }
+        );
+        // A setting for the user to select which compendium contains equipment like armor and weapons
+        game.settings.register(Constants.MODULE_ID, Constants.settings.equipment_compendium,
+            {
+                name: "SHOP_GEN.settings.equipment_compendium.name",
+                hint: "SHOP_GEN.settings.equipment_compendium.hint",
+                scope: "client",
+                requiresReload: false,
+                type: String,
+                choices: getCompendiumsOfType("item"),
+                config: true,
+                default: ""
             }
         );
     }
