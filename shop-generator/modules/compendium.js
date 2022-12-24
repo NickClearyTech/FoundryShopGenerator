e1349f00b5e9b86a6f728d47eab95faa4aa6c0fe
Nick Cleary <nicholascleary01@gmail.com>
2022-12-23 21:19:38 -0500

    Updating file structure slightly and begging work on pricing overrides

--- shop-generator/modules/compendium.js
+++ shop-generator/modules/compendium.js
@@ -1,15 +1,21 @@
 import {getFreshMapByRarity} from "./utils/compendium_utils.js";
+import {consoleLogging} from "./utils/logging.js";
 
-export async function mapCompendiumContentsToRarity(compendiumName) {
-    const itemMap = getFreshMapByRarity();
+export async function mapCompendiumContentsToRarity(compendiumName, shopType) {
+    const itemMap = getFreshMapByRarity(shopType);
     const compendium = game.packs.get(compendiumName);
     let items = await compendium.getDocuments();
+    let rarityOrLevel = "rarity";
+    if (shopType == "spell") {
+        rarityOrLevel = "level";
+    }
     for (const item of items) {
         // Check the rarity is not an empty string
-        if (!item.system.rarity.toLowerCase()) {
-            console.error(`${item.name} does not have a rarity`);
+        if (!item.system[rarityOrLevel].toString().toLowerCase()) {
+            consoleLogging(`${item.name} does not have a ${rarityOrLevel}`, "error");
         }
-        itemMap.get(item.system.rarity.toLowerCase()).push(item);
+        // We've gotta convert to string here just to prevent issues with the fact that spell levels are an integer
+        itemMap.get(item.system[rarityOrLevel].toString().toLowerCase()).push(item);
     }
     return itemMap;
 }
---
+++ shop-generator/modules/values.js
@@ -0,0 +1,67 @@
+/**
+ * Declares a handful of default values
+ */
+export class Constants {
+    static MODULE_ID = "shop-generator";
+
+    static VERSION_NUMBER = "0.0.1";
+
+    static playerFlag = "shop_presents";
+
+    static valid_compendium_types = [
+        "actor",
+        "item",
+        "card stack",
+        "journal entry",
+        "macro",
+        "playlist",
+        "rollable table",
+        "scene",
+        "adventure"
+    ];
+
+    static settings = {
+        potion_compendium: "PotionCompendium",
+        spell_compendium: "SpellCompendium",
+        magic_item_compendium: "MagicItemCompendium",
+        equipment_compendium: "EquipmentCompendium",
+        price_override: "PriceOverride"
+    };
+
+    static rarities = {
+        common: "Common",
+        uncommon: "Uncommon",
+        rare: "Rare",
+        veryrare: "Very Rare",
+        legendary: "Legendary"
+    };
+
+    static validLogSeverities = ["log", "warn", "error", "info"];
+
+    static validShopTypes = ["spell", "potion", "magic_item", "equipment"];
+
+    static templates = {
+        shop_generator: `modules/${this.MODULE_ID}/templates/shop-generator.hbs`
+    };
+
+    static spellLevels = {
+        0: "Cantrip",
+        1: "1st Level",
+        2: "2nd Level",
+        3: "3rd Level",
+        4: "4th Level",
+        5: "5th Level",
+        6: "6th Level",
+        7: "7th Level",
+        8: "8th Level",
+        9: "9th Level"
+    };
+
+    static validPriceOverrideFields = ["price"];
+}
+
+export class RuntimeValues {
+    // We assume the price override to be invalid to be safe
+    static validPriceOverride = false;
+    static priceOverride = null;
+}
